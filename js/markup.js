c9ed02afad07d76e9f127a442716648a62e8dd46
vova_lovalova <rudchenko.vova@mail.ru>
2022-10-25 11:25:29 +0800

    доработки в проект дз 7

--- js/markup.js
+++ js/markup.js
@@ -1,12 +1,48 @@
 import {generateData} from './data.js';
 const data = generateData();
 
-console.log(data);
+// console.log(data);
 
-function getType (newData) {
-  let type;
-  newData.forEach((card) => {
-    switch (card.offer.type) {
+// function getType (newData) {
+//   let type;
+//   newData.forEach((card) => {
+//     switch (card.offer.type) {
+//       case 'flat':
+//         type = 'Квартира';
+//         break;
+//       case 'bungalow':
+//         type = 'Бунгало';
+//         break;
+//       case 'house':
+//         type = 'Дом';
+//         break;
+//       case 'palace':
+//         type = 'Дворец';
+//         break;
+//       case 'hotel':
+//         type = 'Отель';
+//         break;
+//     }
+//   });
+//   return type;
+// }
+
+const map = document.querySelector('#map-canvas');
+const cardTemplate = document.querySelector('#card').content;
+
+data.forEach((card) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__avatar').src = card.autor.avatar;
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.addres;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + ' ₽/ночьe';
+
+  // В блок .popup__type выведите тип жилья offer.type, сопоставив с подписями: - ПОЛУЧИЛОСЬ НЕВЕРНО
+
+  cardElement.querySelector('.popup__type').innerHTML = '';
+  card.offer.type.forEach((offerType) => {
+    let type;
+    switch (offerType) {
       case 'flat':
         type = 'Квартира';
         break;
@@ -23,22 +59,10 @@ function getType (newData) {
         type = 'Отель';
         break;
     }
+    cardElement.querySelector('.popup__type').textContent = type;
   });
-  return type;
-}
 
-const map = document.querySelector('#map-canvas');
-const cardTemplate = document.querySelector('#card').content;
-
-data.forEach((card) => {
-  const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.popup__avatar').src = card.autor.avatar;
-  cardElement.querySelector('.popup__title').textContent = card.offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = card.offer.addres;
-  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + ' ₽/ночьe';
-
-  // В блок .popup__type выведите тип жилья offer.type, сопоставив с подписями: - ПОЛУЧИЛОСЬ НЕВЕРНО
-  cardElement.querySelector('.popup__type').textContent = getType(data);
+  // cardElement.querySelector('.popup__type').textContent = getType(data);
 
   cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
