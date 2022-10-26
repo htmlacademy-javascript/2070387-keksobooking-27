c7c5a53786fa9d6ba42c80b1d043917306a71af1
vova_lovalova <rudchenko.vova@mail.ru>
2022-10-26 16:01:33 +0800

    1 часть дз по модулю 7

--- js/markup.js
+++ js/markup.js
@@ -1,67 +1,31 @@
 import {generateData} from './data.js';
 const data = generateData();
 
-// console.log(data);
-
-// function getType (newData) {
-//   let type;
-//   newData.forEach((card) => {
-//     switch (card.offer.type) {
-//       case 'flat':
-//         type = 'Квартира';
-//         break;
-//       case 'bungalow':
-//         type = 'Бунгало';
-//         break;
-//       case 'house':
-//         type = 'Дом';
-//         break;
-//       case 'palace':
-//         type = 'Дворец';
-//         break;
-//       case 'hotel':
-//         type = 'Отель';
-//         break;
-//     }
-//   });
-//   return type;
-// }
-
 const map = document.querySelector('#map-canvas');
 const cardTemplate = document.querySelector('#card').content;
 
-data.forEach((card) => {
+const getType = function (type) {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+    case 'hotel':
+      return 'Отель';
+  }
+};
+
+const getNewCardElement = function (card) {
   const cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector('.popup__avatar').src = card.autor.avatar;
   cardElement.querySelector('.popup__title').textContent = card.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = card.offer.addres;
-  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + ' ₽/ночьe';
-
-  // В блок .popup__type выведите тип жилья offer.type, сопоставив с подписями: - ПОЛУЧИЛОСЬ НЕВЕРНО
-
-  cardElement.querySelector('.popup__type').innerHTML = '';
-  card.offer.type.forEach((offerType) => {
-    let type = ' ';
-    switch (offerType) {
-      case 'flat':
-        type = 'Квартира';
-        break;
-      case 'bungalow':
-        type = 'Бунгало';
-        break;
-      case 'house':
-        type = 'Дом';
-        break;
-      case 'palace':
-        type = 'Дворец';
-        break;
-      case 'hotel':
-        type = 'Отель';
-        break;
-    }
-    cardElement.querySelector('.popup__type').textContent = type;
-  });
-
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + ' ₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = getType(card.offer.type);
   cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
 
@@ -86,6 +50,10 @@ data.forEach((card) => {
     cardElement.querySelector('.popup__photos').appendChild(photo);
   });
 
-  map.appendChild(cardElement);
-});
+  return cardElement;
+};
+
+const firstCard = getNewCardElement(data[0]);
+
+map.appendChild(firstCard);
 
